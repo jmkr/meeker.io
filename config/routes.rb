2e0716ddfb9912d30fb1e765686bd5dff60ce5cf
John Meeker <jmeeker516@gmail.com>
2013-12-09 13:04:38 -0800

    Update resume link

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ MeekerIo::Application.routes.draw do
   root :to => "home#index"
   match '/photos' => 'home#photos'
   match '/blog'  => redirect("http://blog.meeker.io"), :as => :blog
-  match '/resume' => redirect('https://dl.dropbox.com/u/19475876/MeekerJohn_Resume_v2.pdf'), :as => :resume
+  match '/resume' => redirect('https://dl.dropboxusercontent.com/u/19475876/MeekerJohn_Resume.pdf'), :as => :resume
 end
