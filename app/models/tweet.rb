caa8cf7f1013399bced383d939545684c99a7f2b
John Meeker <jmeeker516@gmail.com>
2014-02-11 09:26:53 -0800

    Fix for twitter API now enforcing SSL connections

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -27,7 +27,8 @@ class Tweet
       :consumer_key=>ENV["TWITTER_CONSUMER_KEY"],
       :consumer_secret=>ENV["TWITTER_CONSUMER_SECRET_KEY"],
       :token=>ENV["TWITTER_ACCESS_TOKEN"],
-      :token_secret=>ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+      :token_secret=>ENV["TWITTER_ACCESS_TOKEN_SECRET"],
+      :ssl=>true
     })
   end
 end
