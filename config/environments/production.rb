9dac8230d45875b8ef0079bc3759431c481f78a6
John Meeker <jmeeker516@gmail.com>
2015-10-06 21:50:06 -0700

    Config fun pt 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,13 +11,13 @@ MeekerIo::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_files = false
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
