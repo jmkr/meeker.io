7ea26aa8c63524aeeeef58c97afa15cc52258b8c
John Meeker <jmeeker@paypal.com>
2022-02-24 13:44:27 -0800

    WIP layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,6 @@ class HomeController < ApplicationController
   def index
 
   end
-  def photos
-
-  end
   def resume
   	
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 MeekerIo::Application.routes.draw do
   root :to => "home#index"
-  get '/photos' => 'home#photos'
-  get '/blog'  => redirect("http://blog.meeker.io"), :as => :blog
   get '/resume' => redirect('https://www.dropbox.com/s/6vpodceec28tgkp/MeekerJohn_Resume_Feb2019.pdf?dl=0'), :as => :resume
 end
