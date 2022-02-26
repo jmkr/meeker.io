7b714bde9cdc2d35352ede58bee35ce3a8671b8c
John Meeker <jmeeker@paypal.com>
2022-02-26 14:01:40 -0700

    update resume link

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 MeekerIo::Application.routes.draw do
   root :to => "home#index"
-  get '/resume' => redirect('https://www.dropbox.com/s/6vpodceec28tgkp/MeekerJohn_Resume_Feb2019.pdf?dl=0'), :as => :resume
+  get '/resume' => redirect('https://www.dropbox.com/s/l0icyfe4m3yx06c/MeekerJohn_Resume_Feb2022.pdf?dl=0'), :as => :resume
 end
